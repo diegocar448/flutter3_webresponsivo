b1a99471b69c6184943a5619eb0653d83bff460f
Diego Cardoso <diegocar448@hotmail.com>
2024-01-24 11:21:32 -0300

    Criando topo para desktop e tablets

--- lib/pages/home/widgets/sections/top_section.dart
+++ lib/pages/home/widgets/sections/top_section.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutterweb/pages/home/widgets/custom_search_field.dart';
 
 
 
@@ -16,7 +17,7 @@ class TopSection extends StatelessWidget {
             child: Stack(
               children: [
                 AspectRatio(
-                  aspectRatio: 3.2,
+                  aspectRatio: 3.4,
                   child: Image.network(
                       'https://images.pexels.com/photos/892757/pexels-photo-892757.jpeg?auto=compress&cs=tinysrgb&dpr=26h=650Sw=940',
                       fit: BoxFit.cover,
@@ -29,9 +30,31 @@ class TopSection extends StatelessWidget {
                       color: Colors.black,
                       elevation: 8,
                       child:Container(
-                        width: 400,
-                        height: 400,
-                      ),
+                        padding: const EdgeInsets.all(22),
+                        width: 450,
+                        child: Column(
+                          children: [
+                            const Text(
+                              'Flutter Web e Mobile Layout',
+                              style: TextStyle(
+                                fontSize: 40,
+                                fontWeight: FontWeight.bold,
+                                color: Colors.white,
+                              ),
+                            ),
+                            const SizedBox(height: 8,),
+                            const Text(
+                              'Layout Flutter para telas de notebooks e tablets, com responsibidade total, mantendo o layout para varios tamanhos de telas',
+                              style: TextStyle(
+                                fontSize: 18,
+                                color: Colors.white,
+                              ),
+                            ),
+                            const SizedBox(height: 16,),
+                            CustomSearchField()
+                          ],
+                        ),
+                      )
                     ),
                 ),
               ],
