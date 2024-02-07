2936e9fdf2436c9cb0b6d9fc8f2473da14a8481d
Diego Cardoso <diegocar448@hotmail.com>
2024-02-07 15:36:15 -0300

    AutoSize

--- lib/pages/home/widgets/course_item.dart
+++ lib/pages/home/widgets/course_item.dart
@@ -1,21 +1,25 @@
 
+import 'package:auto_size_text/auto_size_text.dart';
 import 'package:flutter/material.dart';
 
 class CourseItem extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Column(
-        crossAxisAlignment: CrossAxisAlignment.stretch,
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: [
         Image.network(
           'https://images.pexels.com/photos/892757/pexels-photo-892757.jpeg?auto=compress&cs=tinysrgb&dpr=26h=650Sw=940',
           fit: BoxFit.fitWidth,
+          height: 50,
         ),
         const SizedBox(height: 4),
         Flexible(
           child: LayoutBuilder(
             builder: (_, constraints) {
-              return Text(
+              return AutoSizeText(
                 'Criação de Apps Android e iOS com Flutter - Crie 16 Apps',
+                minFontSize: 12,
                 style: TextStyle(
                   fontWeight: FontWeight.bold,
                   fontSize: 15,
@@ -25,7 +29,7 @@ class CourseItem extends StatelessWidget {
             },
           ),
         ),
-        Text(
+        AutoSizeText(
           'Diego',
           style: TextStyle(color: Colors.grey),
         ),
